07ab5db0198d1092caff57e5fb125526b542771c
stas7t <stas7t@gmail.com>
2017-12-04 11:02:22 +0200

    Day 2 puzzle solved

---
+++ Day02/2.rb
@@ -0,0 +1,20 @@
+checksum1 = 0
+checksum2 = 0
+
+def row_result(arr)
+  arr.sort! { |x, y| y <=> x }.each_with_index do |value1, index|
+    sub_arr = arr[index + 1, arr.length - index]
+    sub_arr.each do |value2|
+      return value1 / value2  if (value1 % value2) == 0
+    end
+  end
+end
+
+File.readlines('input.txt').each do |line|
+  arr = line.split(' ').map{ |value| value.to_i }
+  checksum1 += arr.max - arr.min
+  checksum2 += row_result(arr)
+end
+
+puts "Part One puzzle answer is #{checksum1}" # 53460
+puts "Part Two puzzle answer is #{checksum2}" # 282
